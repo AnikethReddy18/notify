b57f982388985e2e5e6ecb65c32c613011e1caa6
sunny <sunnyrockzzz1811@gmail.com>
2024-08-21 19:36:21 +0530

    Implement Drag Handling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Card = ({text}) => {
-  const style = 'bg-white select-none inline' 
-  + ' m-2 p-2'
+
+  const [position, setPosition] = useState({x: 0, y: 0})
+  const [prevPosition, setPrevPosition] = useState({x: 0, y: 0})
+
+  const dragEndHandler = (e)=>{
+    const currPosition = {x: e.screenX, y: e.screenY}
+    const diff = {
+      x: position.x + (currPosition.x - prevPosition.x),
+      y: position.y + (currPosition.y - prevPosition.y)
+    }
+    
+    setPosition(diff)
+  }
+
+  const dragStartHandler = (e)=>{
+    setPrevPosition({x: e.screenX, y: e.screenY})
+  }
+
+  const cardStyle = {
+    position: 'relative',
+    left: `${position.x}px`,
+    top: `${position.y}px`,
+    backgroundColor: 'white',
+    margin: '8px',
+    padding: '8px',
+    display: 'inline-block',
+  };
 
   return (
-    <div className={style}>
+    <div style={cardStyle} draggable onDragStart={dragStartHandler} onDragEnd={dragEndHandler}>
       {text}
     </div>
   )
